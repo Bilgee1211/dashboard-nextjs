dc8bb14d6bf721c251d1e0cedbc38c48c389b9d0
Bilegbaatar <bilgeebek1211@gmail.com>
2024-11-26 19:47:26 +0900

    init

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,13 @@
+import { Inter, Bilbo_Swash_Caps, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const bilbo_Swash_Caps = Bilbo_Swash_Caps({
+  weight: "400",
+  subsets: ["latin"],
+});
